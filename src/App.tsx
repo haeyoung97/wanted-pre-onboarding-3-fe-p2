13bd30f194f15a77952cb99b9fb8ba90f8b2dd1e
haeyoung97 <godud6379@naver.com>
2023-03-15 00:44:26 +0900

    feat: add JWTLogin business logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import JWTLogin from './pages/2-1/JWTLogin'
-import JWTLoginWithLocalStorage from './pages/2-2/JWTLoginWithLocalStorage'
-import AutoLogin from './pages/2-2/AutoLogin'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import JWTLogin from "./pages/2-1/JWTLogin";
+import JWTLoginWithLocalStorage from "./pages/2-2/JWTLoginWithLocalStorage";
+import AutoLogin from "./pages/2-2/AutoLogin";
+import "./App.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <JWTLogin/>,
+    element: <JWTLogin />,
   },
   {
     path: "/local-storage",
-    element: <JWTLoginWithLocalStorage/>,
+    element: <JWTLoginWithLocalStorage />,
   },
   {
     path: "/other-page",
-    element: <AutoLogin/>,
+    element: <AutoLogin />,
   },
 ]);
 
@@ -24,7 +24,7 @@ function App() {
     <div className="content">
       <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
